55438c86255643a5e23df8a8b16b925ff6b92f2a
Ruben Verborgh <ruben@verborgh.org>
2016-06-21 20:14:52 -0700

    Simplify query string generation.

--- ldf-client-jquery-url.js
+++ ldf-client-jquery-url.js
@@ -7,7 +7,8 @@ jQuery(function ($) {
     loadStateFromUrl();
     $(window).on('popstate', loadStateFromUrl);
     // Store the UI state in the URL when the UI changes
-    $queryui.on('change', saveStateToUrl);
+    if (history.replaceState)
+      $queryui.on('change', saveStateToUrl);
   });
 
   // Loads the UI state from the URL
@@ -25,16 +26,21 @@ jQuery(function ($) {
 
   // Stores the current UI state in the URL
   function saveStateToUrl() {
-    var url = location.href.replace(/#.*/, ''),
+    var queryString = [],
         options = $queryui.queryui('option'),
         datasources = options.selectedDatasources || [],
         defaultDatasource = (options.datasources[0] || {}).url,
         hasDefaultQuery = options.query === (options.queries[0] || {}).sparql,
         hasDefaultDatasource = datasources.length === 0 ||
                                (datasources.length === 1 && datasources[0] === defaultDatasource);
-    if (!hasDefaultDatasource || !hasDefaultQuery)
-      url += '#datasources=' + datasources.map(encodeURIComponent).join(';') +
-             (hasDefaultQuery ? '' : '&query=' + encodeURIComponent(options.query || ''));
-    history.replaceState && history.replaceState(null, null, url);
+    // Set query string options
+    if (!hasDefaultDatasource)
+      queryString.push('datasources=' + datasources.map(encodeURIComponent).join(';'));
+    if (!hasDefaultQuery)
+      queryString.push('query=' + encodeURIComponent(options.query || ''));
+
+    // Compose new URL with query string
+    queryString = queryString.length ? '#' + queryString.join('&') : '';
+    history.replaceState(null, null, location.href.replace(/(?:#.*)?$/, queryString));
   }
 });
